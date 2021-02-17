b32837b637e54f2be3a242cf21f9a13370abfcde
Junaid <JSidat95@gmail.com>
2021-02-17 11:30:01 +0000

    change made

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,21 +3,23 @@ package fizzbuzz;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		fizzBuzz(9);
-		fizzBuzz(10);
-		fizzBuzz(15);
+		System.out.println(fizzBuzz(9));
+		System.out.println(fizzBuzz(10));
+		System.out.println(fizzBuzz(15));
+		System.out.println(fizzBuzz(19));
 
 	}
 
-	public static void fizzBuzz(int a) {
+	public static String fizzBuzz(int a) {
 		if (a % 3 == 0 && a % 5 == 0) {
-			System.out.println("FizzBuzz");
+			return "FizzBuzz";
 		} else if (a % 3 == 0) {
-			System.out.println("Fizz");
-			;
+			return "Fizz";
 		} else if (a % 5 == 0) {
-			System.out.println("Buzz");
-			;
+			return "Buzz";
+		} else {
+			return String.valueOf(a);
+
 		}
 
 	}
